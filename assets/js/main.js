e898c617863447fbdd7dc36c7d611a7265551fe2
Shihab <73121069+maker-shihab@users.noreply.github.com>
2021-11-07 15:35:52 +0600

    Planing Page Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,23 @@ $( document ).ready(function() {
     $(".app-header-nav").removeClass("open");
   });
 });
+// Carousel 
+$( document ).ready(function() {
+  $('.eventSlider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+  });
+});
