46e61b3a557ece67deea00efe9fec48a04bfcac6
Shihab <73121069+maker-shihab@users.noreply.github.com>
2023-10-26 23:09:34 +0600

    Add some packages

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Start from './components/Start';
+import './styles/styles.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Start />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
